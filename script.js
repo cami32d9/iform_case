a15ff25f9fad1aa4290bbcae3ecb3ea05e5eeb10
Camilla Gejl Olsen <cami32d9@stud.kea.dk>
2019-10-09 17:30:29 +0200

    Added back buttons and fied invalidity to only happen after trying to submit

--- script.js
+++ script.js
@@ -17,24 +17,45 @@ function start() {
     console.log(currentCheckoutSection);
 
     const form = document.querySelector("form");
+
+    // FORWARD BUTTONS
     const forwardButtons = document.querySelectorAll(".forward");
     forwardButtons.forEach(button => {
         console.log("addListener");
         button.addEventListener("click", function(e) {
-            if(form.checkValidity()) {
+
+            if (form.checkValidity() === false) {
+                currentCheckoutSection.classList.add("invalidated");
+            }
+
+            if (form.checkValidity()) {
                 e.preventDefault();
+                currentCheckoutSection.classList.remove("invalidated");
 
-                const gotoDestination = button.getAttribute("data-goto");
+                const checkoutDestination = button.getAttribute("data-goto");
 
-                if (gotoDestination !== "thankyou") {
+                if (checkoutDestination !== "thankyou") {
 
-                goToNextCheckoutPage(gotoDestination);
+                goToDestination(checkoutDestination);
                 }
             }
         });
     });
 
-    function goToNextCheckoutPage(nextPage) {
+    // BACK BUTTONS
+    const backButtons = document.querySelectorAll(".back");
+    backButtons.forEach(button => {
+        console.log("addListener");
+        button.addEventListener("click", function(e) {
+                e.preventDefault();
+
+                const checkoutDestination = button.getAttribute("data-goto");
+
+                    goToDestination(checkoutDestination);
+                });
+    });
+
+    function goToDestination(nextPage) {
         console.log(nextPage);
 
         let nextCheckoutSection = document.querySelector(`#${nextPage}`);
